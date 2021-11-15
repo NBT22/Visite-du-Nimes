2077f4dd4df439efd8d3d73fba92e61c50cebb17
NBT22 <noah.thwing@gmail.com>
2021-11-15 17:23:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-var Présentation = getElementByID("Présentation");
-var PrésentationAudio = getElementByID("Présentation Audio");
-
-function clickHandler() {
-  PrésentationAudio.play();
-}
-
-function endHandler() {
-  alert('ended');
-}
-
-Présentation.addEventListener('click', clickHandler, false);
-PrésentationAudio.addEventListener('ended', endHandler, false);
+var audio = new Audio("./Audio/Présentation.wav");
+audio.play();
