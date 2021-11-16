8b7643df3ce54f235ea87aeeac19bfdf0cc2de1f
NBT22 <noah.thwing@gmail.com>
2021-11-15 18:25:39 -0600

    f

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 var Présentation = new Audio("./Audio/Présentation.wav");
+
 Présentation.addEventListener('ended', endHandler, false);
 function endHandler() {
   window.location.href = "./Slides/";
+}
+
+var Accueil = new Audio("./Audio/Accueil.wav");
+
+Accueil.addEventListener('ended', endHandler, false);
+function endHandler() {
+  window.location.href = "./";
 }
