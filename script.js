47142aa26fb29fb87f616cb2caafe1f514d5059a
NBT22 <91098724+NBT22@users.noreply.github.com>
2021-11-17 09:29:15 -0600

    might work right now

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var Présentation = new Audio("./Audio/Présentation.wav");
       
-      Présentation.addEventListener('ended', endHandler, false);
-      function endHandler() {
+      Présentation.addEventListener('ended', présentationEndHandler, false);
+      function présentationEndHandler() {
         window.location.href = "./Slides/";
       }
 
 var CIC = new Audio("./Audio/CIC Ad.wav");
       
-      CIC.addEventListener('ended', endHandler, false);
-      function endHandler() {
+      CIC.addEventListener('ended', CICEndHandler, false);
+      function CICEndHandler() {
         window.location.href = "https://www.cic.fr/fr/agences/particuliers/10096/18057/00/000?y_source=1_MTQxNjA0MTAtNzE1LWxvY2F0aW9uLndlYnNpdGU%3D";
       }
