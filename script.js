58216299cb77989a6c856b107f0c60db01b44ad0
NBT22 <noah.thwing@gmail.com>
2021-11-16 19:09:16 -0600

    goes to website now

--- script.js
+++ script.js
@@ -9,5 +9,5 @@ var CIC = new Audio("./Audio/CIC Ad.wav");
       
       CIC.addEventListener('ended', endHandler, false);
       function endHandler() {
-        window.location.href = "./Slides/";
+        window.location.href = "https://www.cic.fr/fr/agences/particuliers/10096/18057/00/000?y_source=1_MTQxNjA0MTAtNzE1LWxvY2F0aW9uLndlYnNpdGU%3D";
       }
