292ed78659b70acfec84f770da3cba4be20fa42f
NBT22 <noah.thwing@gmail.com>
2021-11-18 18:25:54 -0600

    lots

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var Présentation = new Audio("./Audio/Présentation.wav");
-      
-      Présentation.addEventListener('ended', présentationEndHandler, false);
-      function présentationEndHandler() {
+const Presentation = new Audio("./Audio/Présentation.wav");
+
+Presentation.addEventListener('ended', presentationEndHandler, false);
+      function presentationEndHandler() {
         window.location.href = "./Slides/";
       }
 
-var CIC = new Audio("./Audio/CIC Ad.wav");
-      
-      CIC.addEventListener('ended', CICEndHandler, false);
+const CIC = new Audio("./Audio/CIC Ad.wav");
+
+CIC.addEventListener('ended', CICEndHandler, false);
       function CICEndHandler() {
         window.location.href = "https://www.cic.fr/fr/agences/particuliers/10096/18057/00/000?y_source=1_MTQxNjA0MTAtNzE1LWxvY2F0aW9uLndlYnNpdGU%3D";
       }
+
+const Boulangerie = new Audio("./Audio/Boulangerie.wav");
